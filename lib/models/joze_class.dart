f1061613d45d858042dc50b37e5ddc342b7b7086
bathaei <javadbat@gmail.com>
2018-03-29 11:28:18 +0430

    bvb

--- lib/models/joze_class.dart
+++ lib/models/joze_class.dart
@@ -13,19 +13,44 @@ class Hezb{
 List<Joze> getJozeList(){
 
   var data = new List<Joze>();
-  var joze1 = new Joze();
-  joze1= new Joze();
-  joze1.jozeId = 1;
-  joze1.startPage = 0;
-  joze1.endPage = 1;
-  joze1.hezbList = new List<Hezb>();
-  var hezb1 = new Hezb();
-  hezb1.hezbId = 1;
-  hezb1.startPage = 0;
-  hezb1.endPage = 1;
-  joze1.hezbList.add(hezb1);
+  /* start joz define */
+    var joze1 = new Joze();
+    joze1= new Joze();
+    joze1.jozeId = 1;
+    joze1.startPage = 1;
+    joze1.endPage = 1;
+    joze1.hezbList = new List<Hezb>();
+    //define hzb
+    {
+      var hezb1 = new Hezb();
+      hezb1.hezbId = 1;
+      hezb1.startPage = 1;
+      hezb1.endPage = 7;
+      joze1.hezbList.add(hezb1);
+    //define hzb
+      var hezb2 = new Hezb();
+      hezb2.hezbId = 2;
+      hezb2.startPage = 7;
+      hezb2.endPage = 11;
+      joze1.hezbList.add(hezb2);
+    //define hzb
+      var hezb3 = new Hezb();
+      hezb3.hezbId = 3;
+      hezb3.startPage = 11;
+      hezb3.endPage = 17;
+      joze1.hezbList.add(hezb3);
+    //define hzb
+      var hezb4 = new Hezb();
+      hezb4.hezbId = 4;
+      hezb4.startPage = 17;
+      hezb4.endPage = 22;
+      joze1.hezbList.add(hezb4);
+    //end of define hezb
+    }
   data.add(joze1);
+  /* end of joze define*/
   return data;
-  //data[0].hezbList
+}
+void defineHezbForjoze(Joze joze ,int hezb1StartPage, int hezb2StartPage,int hezb3StartPage,int hezb4StartPage){
 
 }
